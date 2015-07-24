39cd5697f8715af8f7084a09a32d511fea6c8289
Trey Huffine <treyhuffine@gmail.com>
2015-07-24 10:57:08 -0700

    Set up basic React file

---
+++ script.jsx
@@ -0,0 +1,22 @@
+var Hello = React.createClass({
+  render: function() {
+    var names = ["Gerald", "Dan", "Trey", "Samer", "Sheryl", "Elijah", "Ryano"];
+    var greetings = ["Hello", "Welcome", "What's happenin"];
+
+    var hellos = [];
+
+    for (var i = 0; i < names.length; i++) {
+      hellos.push(<h1>{greetings[i%greetings.length]} {names[i]}</h1>);
+    }
+    return (
+      <div>
+        {hellos}
+      </div>
+    );
+  }
+});
+
+React.render(
+  <div><Hello /></div>,
+  document.getElementById("root")
+);
