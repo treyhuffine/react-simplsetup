6c619627345c211a160202ea2aeae3e7b5fca14b
Trey Huffine <treyhuffine@gmail.com>
2015-07-24 15:08:10 -0700

    Move all features to component and finish MVP

--- script.jsx
+++ script.jsx
@@ -1,30 +1,64 @@
-var Hello = React.createClass({
+var OneUserGreeting = React.createClass({
+  render: function() {
+    return <li>Hello {this.props.name}</li>
+  }
+});
+
+var GreetingForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.greet(this.refs.name2greet);
+  },
   render: function() {
     return (
+      <form>
+        <input placeholder="Name" ref="name2greet" />
+        <button onClick={this.handleSubmit}>Greet</button>
+      </form>
+    )
+  }
+});
+
+var ListOfGreetings = React.createClass({
+  render: function() {
+    var usersLIs = this.props.users.map(function(name, i) {
+      return <OneUserGreeting name={name} key={i}/>
+    });
+    return (
       <div>
-        {this.props.greet} {this.props.name}
+        <ul>
+          {usersLIs}
+        </ul>
       </div>
     );
   }
 });
 
 var App = React.createClass({
+  getInitialState: function() {
+    return { users: [] }
+  },
+  // name: this.refs.name2greet
+  greet: function(nameInput) {
+    this.setState({
+      users: this.state.users.concat(nameInput.value)
+    }, function() {
+        nameInput.value = '';
+       }
+    );
+  },
   render: function() {
-    var component = this, collection = component.props.collection;
-
-    var greets = Object.keys(collection).map(function(person) {
-      return (<Hello name={person} greet={collection[person]} />);
-    });
     return (
       <div>
-        <h1>React Example</h1>
-        {greets}
+        <GreetingForm greet={this.greet} />
+        <hr />
+        <ListOfGreetings users={this.state.users} />
       </div>
     )
   }
 })
 
 React.render(
-  <div><App collection={ {gerald: 'hi', ryan: 'hello', trey: 'yo'} }/></div>,
+  <div><App /></div>,
   document.getElementById("root")
 );
